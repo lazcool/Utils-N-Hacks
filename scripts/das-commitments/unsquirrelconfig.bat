a3314875a87b63a3135d0f41262cd12a85ca28ea
phildavies <github@phildavies.co.uk>
2018-02-15 08:52:41 +0000

    fix

--- scripts/das-commitments/unsquirrelconfig.bat
+++ scripts/das-commitments/unsquirrelconfig.bat
@@ -1,4 +1,4 @@
 
-copy C:\work\configs\SFA.DAS.Commitments.AddEpaToApprenticeships.WebJob\App.config C:\git\das-commitments\src\SFA.DAS.Commitments.AddEpaToApprenticeships.WebJob
-copy c:\work\configs\SFA.DAS.CommitmentPayments.WebJob\App.config C:\git\das-commitments\src\SFA.DAS.CommitmentPayments.WebJob\App.config
-copy c:\work\configs\SFA.DAS.Commitments.AcademicYearEndProcessor.WebJob\App.config C:\git\das-commitments\src\SFA.DAS.Commitments.AcademicYearEndProcessor.WebJob\App.config
+copy C:\work\configs\das-commitments\SFA.DAS.Commitments.AddEpaToApprenticeships.WebJob\App.config C:\git\das-commitments\src\SFA.DAS.Commitments.AddEpaToApprenticeships.WebJob
+copy c:\work\configs\das-commitments\SFA.DAS.CommitmentPayments.WebJob\App.config C:\git\das-commitments\src\SFA.DAS.CommitmentPayments.WebJob\App.config
+copy c:\work\configs\das-commitments\SFA.DAS.Commitments.AcademicYearEndProcessor.WebJob\App.config C:\git\das-commitments\src\SFA.DAS.Commitments.AcademicYearEndProcessor.WebJob\App.config
